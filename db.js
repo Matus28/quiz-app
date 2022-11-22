f967c40ab2f8522d4785b6f4a207518087560a50
Matus Tatarko <matus.tatarko28@gmail.com>
2022-11-22 15:02:48 +0100

    change in db js file, correcting of returning after error ocurred

--- db.js
+++ db.js
@@ -14,6 +14,7 @@ function query(sqlQuery, arrayOfValues) {
       if(err) {
         console.log(err);
         reject(`Database error!`);
+        return;
       }
       return resolve(rows);
     });
